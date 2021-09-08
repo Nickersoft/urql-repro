a4066f48a0ac43eb86f988680a1722f9765bbcbe
Tyler Nickerson <nickersoft@gmail.com>
2021-09-08 14:15:47 -0700

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { createClient, defaultExchanges } from '@urql/core';
+	import { setClient, query, operationStore } from '@urql/svelte';
+
+	const client = createClient({
+		url: 'https://0ufyz.sse.codesandbox.io',
+		exchanges: [...defaultExchanges]
+	});
+
+	setClient(client);
+
+	const todos = query(
+		operationStore(`
+  query {
+    todos {
+      id
+      text
+      complete
+    }
+  }
+  `)
+	);
+</script>
+
+{#if $todos.data}
+	{JSON.stringify($todos.data)}
+{/if}
